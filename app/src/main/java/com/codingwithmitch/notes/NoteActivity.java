bac4874faedb73221724341403c249abb53d451d
Andre (Desktop) <ahochschulte.apple@gmail.com>
2020-10-04 16:45:35 +0200

    Setting Initial Note Properties

--- app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
+++ app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
@@ -13,15 +13,50 @@ public class NoteActivity extends AppCompatActivity {
 
     private static final String TAG = "NoteActivity";
 
+    //ui components
+    private LineEditText mLinedEditText;
+    private EditText mEditTitle;
+    private TextView mViewTitle;
+
+    //vars
+    private boolean mIsNewNote;
+    private Note mInitialNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        mLinedEditText = findViewById(R.id.note_text);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mViewTitle = findViewById(R.id.note_text_title);
 
-        if(getIntent().hasExtra("selected_note")){
-            Note note = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: "+ note);
+        if(getIncomingIntent()) {
+            //this is a new note (EDIT MODE)
+            setNewNoteProperties();
+        }else{
+            setNoteProperties();
         }
-        
+
+    }
+
+    private boolean getIncomingIntent(){
+        if(getIntent().hasExtra("selected_note")){
+            mInitialNote = getIntent().getParcelableExtra("selected_note");
+            //Log.d(TAG, "getIncomingIntent: "+ incomingNote.toString());
+            mIsNewNote = false;
+            return false;
+        }
+        return true;
+    }
+
+    private void setNoteProperties(){
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
+    }
+
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note TItle");
+        mEditTitle.setText("Note Title");
     }
 }

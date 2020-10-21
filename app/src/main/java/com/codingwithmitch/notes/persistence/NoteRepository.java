b9f78543b9bd732332d96d2549657e45abf33ba5
andre <ahochschulte.apple@gmail.com>
2020-10-21 19:42:41 +0200

    Room Persistence Repository

---
+++ app/src/main/java/com/codingwithmitch/notes/persistence/NoteRepository.java
@@ -0,0 +1,34 @@
+package com.codingwithmitch.notes.persistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.codingwithmitch.notes.models.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private  NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+
+    }
+
+    public void updateNote(Note note){
+
+    }
+
+    public LiveData<List<Note>> retrieveNoteTask(){
+        return null;
+    }
+
+    public void deleteNote(Note note){
+
+    }
+}
